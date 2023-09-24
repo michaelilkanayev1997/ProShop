23aa9e9309a3f881400c16cd583fdee83e3ea8d2
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2023-09-24 20:53:16 +0300

    Added Creating An Order

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -37,6 +37,10 @@ const cartSlice = createSlice({
       state.paymentMethod = action.payload;
       return updateCart(state);
     },
+    clearCartItems: (state, action) => {
+      state.cartItems = [];
+      return updateCart(state);
+    },
     // NOTE: here we need to reset state for when a user logs out so the next
     // user doesn't inherit the previous users cart and shipping
     resetCart: (state) => (state = initialState),
@@ -48,6 +52,7 @@ export const {
   removeFromCart,
   saveShippingAddress,
   savePaymentMethod,
+  clearCartItems,
   resetCart,
 } = cartSlice.actions;
 
